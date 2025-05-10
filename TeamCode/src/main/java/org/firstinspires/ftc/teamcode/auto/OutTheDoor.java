ac2bdcea2e6f043bd0a818aa0525035ef7baec11
pcokis <prestoncokis@gmail.com>
2025-05-10 08:45:25 -0400

    Updated Positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/OutTheDoor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/OutTheDoor.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.trailblazer.drivebase.Drive;
 import org.firstinspires.ftc.teamcode.trailblazer.path.Path;
+import org.fotmrobotics.trailblazer.Pose2D;
 import org.fotmrobotics.trailblazer.Vector2D;
 
 @Autonomous
@@ -16,9 +17,12 @@ public class OutTheDoor extends LinearOpMode {
 
         Path thePath = drive.PathBuilder(new Vector2D(0, 0))
                 .translationalScale(0.5)
-                .point(new Vector2D(0, 150))
-                .point(new Vector2D(110, 150))
-                .point(new Vector2D(110, 245))
+                .point(new Vector2D(2.5, 146))
+                .point(new Vector2D(71, 160.7))
+                .point(new Vector2D(103, 197.5))
+                .point(new Vector2D(11.3, 217.5))
+                .point(new Vector2D(-0.6, 372.7))
+                .point(new Vector2D(2, 1241))
                 .build();
 
         waitForStart();
